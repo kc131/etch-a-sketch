87108335a5115a52258546d37d075765f7e0af0a
kc131 <121075186+kc131@users.noreply.github.com>
2023-06-16 20:52:54 -1000

    16x16 Grid complete

--- etch-a-sketch-script.js
+++ etch-a-sketch-script.js
@@ -1,8 +1,20 @@
 function createSquare() {
     const mainContainer = document.getElementById("main-container");
-    const newSquare = document.createElement('div');
+    let newSquare = document.createElement('div');
     newSquare.classList.add("square-style");
     mainContainer.appendChild(newSquare);
 }
 
-createSquare();
+function createAllSquares() {
+for (i=1; i<=256; i++) {
+    createSquare();
+    /*let tempVar = "square" + i;  //delete this part if you end up not needing it
+    console.log(tempVar); */
+
+}
+
+
+}
+
+
+createAllSquares();
