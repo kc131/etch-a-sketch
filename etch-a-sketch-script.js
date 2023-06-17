fdfe04bb0c37037a137271a790ab1e7e34b8c864
kc131 <121075186+kc131@users.noreply.github.com>
2023-06-16 20:18:53 -1000

    Test square compleat

--- etch-a-sketch-script.js
+++ etch-a-sketch-script.js
@@ -1,2 +1,8 @@
-let testVar = "throw this away";
-console.log(testVar);
+function createSquare() {
+    const mainContainer = document.getElementById("main-container");
+    const newSquare = document.createElement('div');
+    newSquare.classList.add("square-style");
+    mainContainer.appendChild(newSquare);
+}
+
+createSquare();
