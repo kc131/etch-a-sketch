d4277c355c96513f77757e8b07375fa0546a0da1
kc131 <121075186+kc131@users.noreply.github.com>
2023-06-22 14:08:23 -1000

    Complete basic game with ability to change canvas color and ink color as well as an option to reset the ink pattern

--- etch-a-sketch-script.js
+++ etch-a-sketch-script.js
@@ -1,12 +1,18 @@
+const canvasColorChoice = document.getElementById("canvas-color-choice");
+const inkColorChoice = document.getElementById("ink-color-choice");
+const submitButton = document.getElementById("submit-button");
+let currentInkColor = "white";
+let currentCanvasColor = "black";
+
 function createSquare() {
-    const mainContainer = document.getElementById("main-container");
+    const gameCanvas = document.getElementById("game-canvas");
     let newSquare = document.createElement('div');
     newSquare.classList.add("square-style");
-    mainContainer.appendChild(newSquare);
+    gameCanvas.appendChild(newSquare);
 }
 
 function createAllSquares() {
-for (i=1; i<=256; i++) {
+for (i=1; i<=5000; i++) {
     createSquare();
 }
 }
@@ -18,18 +24,78 @@ for (i=0; i<squareCollection.length; i++) {
     let aSquare = squareCollection[i];
     aSquare.addEventListener("mouseover", function() {
         mouseOver(aSquare);
-      });
-
+        });
 };
 
 function mouseOver(chosenDiv) {
-    chosenDiv.style.backgroundColor = "red";
-
+    chosenDiv.style.backgroundColor = currentInkColor;
 }
 
+submitButton.addEventListener("click", resetColors);
 
 
+function resetColors() {
+    const gameCanvas = document.getElementById("game-canvas");
+    let newCanvasColorChoice = canvasColorChoice.value;
+    let newInkColorChoice = inkColorChoice.value;
+    currentInkColor = newInkColorChoice;
+    
+    if (document.getElementById('reset-ink-yes').checked === true) {
+    for (i=0; i<squareCollection.length; i++) {
+        let aSquare = squareCollection[i];
+        aSquare.removeAttribute('style');
+    };
+    };
 
+    switch(newCanvasColorChoice) {
+        case "black":
+            gameCanvas.classList.remove(`bg-${currentCanvasColor}`);
+            gameCanvas.classList.add(`bg-${newCanvasColorChoice}`);
+            currentCanvasColor = newCanvasColorChoice;
+            break;
 
+        case "white":
+            gameCanvas.classList.remove(`bg-${currentCanvasColor}`);
+            gameCanvas.classList.add(`bg-${newCanvasColorChoice}`);
+            currentCanvasColor = newCanvasColorChoice;
+            break; 
+            
+        case "red":
+            gameCanvas.classList.remove(`bg-${currentCanvasColor}`);
+            gameCanvas.classList.add(`bg-${newCanvasColorChoice}`);
+            currentCanvasColor = newCanvasColorChoice;
+            break;
+    
+        case "green":
+            gameCanvas.classList.remove(`bg-${currentCanvasColor}`);
+            gameCanvas.classList.add(`bg-${newCanvasColorChoice}`);
+            currentCanvasColor = newCanvasColorChoice;
+            break;
 
+        case "yellow":
+            gameCanvas.classList.remove(`bg-${currentCanvasColor}`);
+            gameCanvas.classList.add(`bg-${newCanvasColorChoice}`);
+            currentCanvasColor = newCanvasColorChoice;
+            break; 
+                
+        case "blue":
+            gameCanvas.classList.remove(`bg-${currentCanvasColor}`);
+            gameCanvas.classList.add(`bg-${newCanvasColorChoice}`);
+            currentCanvasColor = newCanvasColorChoice;
+            break;
+
+        case "purple":
+            gameCanvas.classList.remove(`bg-${currentCanvasColor}`);
+            gameCanvas.classList.add(`bg-${newCanvasColorChoice}`);
+            currentCanvasColor = newCanvasColorChoice;
+            break;
+
+        case "orange":
+            gameCanvas.classList.remove(`bg-${currentCanvasColor}`);
+            gameCanvas.classList.add(`bg-${newCanvasColorChoice}`);
+            currentCanvasColor = newCanvasColorChoice;
+            break;
+
+    }
+}
 
